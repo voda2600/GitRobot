3b2b2f6a8e80f1b4c46fcb6dee4a6b45bed07f7e
MrRobot <voda2600@mail.ru>
2020-10-03 14:16:43 +0300

    Mondelbrot

--- ConsoleApplication1/ConsoleApplication1/Program.fs
+++ ConsoleApplication1/ConsoleApplication1/Program.fs
@@ -5,8 +5,9 @@ open System
 open System.Windows.Forms
 open System.Drawing
 
-let height = 400.0
-let width = 400.0
+
+let height = 280.0
+let width = 280.0
 let cMax = complex 1.0 1.0
 let cMin = complex -1.0 -1.0
 let rec isInMandelbrotSet (zero, c, cur_iter, iter) =
@@ -14,11 +15,11 @@ let rec isInMandelbrotSet (zero, c, cur_iter, iter) =
         isInMandelbrotSet ( ((zero * zero) + c), c, cur_iter+1, iter )
     else cur_iter
 
-let convertMeasure(x:int, y:int, scale:float, mx:float,my:float) =
+let newCoordinates(x:int, y:int, scale:float, mx:float,my:float) =
        let toX p =
-           (p-(width/2.5))/(width/4.0*scale)
+           (p-100.0)/(100.0*scale)
        let toY p =
-           -(p-(height/2.5))/(height/4.0*scale)
+           -(p-100.0)/(100.0*scale)
        complex (toX(float x) + mx) (toY(float y) + my)
 
 let colorize c =
@@ -29,19 +30,20 @@ let colorize c =
 
 
 let createImage (mx, my, iter,scale) =
-    let image = new Bitmap(400, 400)
+    let image = new Bitmap(Convert.ToInt32(width), Convert.ToInt32(height))
     for x = 0 to image.Width - 1 do
         for y = 0 to image.Height - 1 do
-            let count = isInMandelbrotSet( Complex.Zero, convertMeasure(x,y,scale,mx,my), 0, iter)
+            let count = isInMandelbrotSet( Complex.Zero, newCoordinates(x,y,scale,mx,my), 0, iter)
             if count = iter then
                 image.SetPixel(x,y, Color.Black)
             else
                 image.SetPixel(x,y, colorize( count ) )
     image
-let mutateImage (ans:Form) (i:float)= ans.Paint.Add(fun e -> e.Graphics.DrawImage(createImage(-0.7,0.28,50+Convert.ToInt32(i*0.9),i*3.0), 0, 0))
+
+let mutateImage (ans:Form) (i:float)= ans.Paint.Add(fun e -> e.Graphics.DrawImage(createImage(-0.7,0.28,30+Convert.ToInt32(i*3.0),i*2.0), 0, 0))
 let ans  = new Form()
-//ans.Paint.Add(fun e -> e.Graphics.DrawImage(createImage(-0.7,0.28,100,1.0), 0, 0))
-ans.Size <- new Size(400,440)
+ans.Paint.Add(fun e -> e.Graphics.DrawImage(createImage(-0.7,0.28,100,1.0), 0, 0))
+ans.Size <- new Size(Convert.ToInt32(width), Convert.ToInt32(height))
 
 for i in 1.0..40.0 do
     mutateImage ans i
